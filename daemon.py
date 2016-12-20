ba65b9380cf453ee83f6e2b140941cbba19b7f42
EC2 Default User <ec2-user@ip-10-0-0-92.us-west-2.compute.internal>
2016-12-20 06:45:08 +0000

    test

--- daemon.py
+++ daemon.py
@@ -5,14 +5,14 @@ import os
 
 KILL_PORT = 6666
 
-NUMBER = 3
+NUMBER = 1
 
 container_name = 'client' + str(NUMBER)
 
 kill_command = "docker stop " + container_name
 
 def launch_container():
-	launch_command = "docker run -d -P --name " + str(container_name) + " 571106538810.dkr.ecr.us-west-2.amazonaws.com/gopshyam/amazonlinux python /home/FTC_Project/replica_socket.py " + str(NUMBER) 
+	launch_command = "docker run -d -P --name " + str(container_name) + " 571106538810.dkr.ecr.us-west-2.amazonaws.com/gopshyam/amazonlinux:test python /home/FTC_Project/replica_socket.py " + str(NUMBER) 
 	os.system(launch_command)
 
 def listen_for_signal():
