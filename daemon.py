f9fe27565c3280852f244b2b426c9d6b91fb78f3
EC2 Default User <ec2-user@ip-10-0-0-89.us-west-2.compute.internal>
2016-12-20 06:25:06 +0000

    Step 1 done

---
+++ daemon.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+import socket
+import os
+
+KILL_PORT = 6666
+
+NUMBER = 3
+
+container_name = 'client' + str(NUMBER)
+
+kill_command = "docker stop " + container_name
+
+def launch_container():
+	launch_command = "docker run -d -P --name " + str(container_name) + " 571106538810.dkr.ecr.us-west-2.amazonaws.com/gopshyam/amazonlinux" 
+	os.system(launch_command)
+
+def listen_for_signal():
+	kill_listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	kill_listener.bind(('', KILL_PORT))
+
+	kill_listener.listen(6)
+	while(True):
+		send_sock, addr = kill_listener.accept()
+		message = send_sock.recv(40)
+		if 'KILL' in message:
+			os.system(kill_container)
+			launch_container()
+
+launch_container()
+
+listen_for_signal()
